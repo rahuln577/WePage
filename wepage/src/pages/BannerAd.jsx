c22dbcb3eabc89a52632545c609d00f560ef857c
shyamg090 <samshyam9630@gmail.com>
2024-04-06 19:11:00 +0530

    banner added -0 - 6/4/2024

---
+++ wepage/src/pages/BannerAd.jsx
@@ -0,0 +1,10 @@
+export default function BannerAd() {
+    return (
+        <div className=" w-full my-4 h-[20rem] realtive ">
+            <img src={require('../assets/banner1 (1).jpg')} alt="banner" className='w-full h-full object-cover' />
+            <div className='absolute  h-full'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet veritatis pariatur eveniet facilis vero atque, ea doloremque illo nisi excepturi magni magnam quae odio accusantium fuga eum iusto voluptate ullam similique inventore quaerat aspernatur. Nulla voluptatum quas officia omnis.
+            </div>
+        </div>
+    )
+}
